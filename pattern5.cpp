7b2ba52c73fa15b4deb55f610676c8c62c422d81
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-01-23 21:48:42 +0530

    pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,24 @@
+// Example 1:
+
+// Input: 5
+
+// Output:
+// * * * * *
+// * * * * 
+// * * * 
+// * *  
+// * 
+
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+	}
+};
