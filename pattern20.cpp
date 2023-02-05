212327dc3aef8b6a3e965464d4d925cacda96336
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-02-05 22:01:17 +0530

    Update pattern20.cpp

--- pattern20.cpp
+++ pattern20.cpp
@@ -1,1 +1,44 @@
+// Input: 5
 
+// Output:
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int i,j;
+        for(i=1;i<n;i++){
+            for(j=1;j<=i;j++){
+                cout<<"*";
+            }
+            for(j=1;j<n-i+1;j++){
+                cout<<"  ";
+            }
+            for(j=1;j<=i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i+1;j++){
+                cout<<"*";
+            }
+            for(j=1;j<i;j++){
+                cout<<"  ";
+            }
+            for(j=1;j<=n-i+1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
